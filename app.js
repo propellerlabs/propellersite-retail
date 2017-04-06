f688e0e94a06ac7d53555728482f6e1b5118c7cc
John Gibbons <johngibbons10@gmail.com>
2017-04-05 18:15:39 -0700

    send email for start a project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
+var bodyParser = require('body-parser');
 
 var options = {
   auth: {
@@ -11,17 +12,15 @@ var options = {
 
 var client = nodemailer.createTransport(sgTransport(options));
 
-var email = {
-  from: 'webform@propellerlabs.co',
-  to: 'john@propellerlabs.co',
-  subject: 'Hello',
-  text: 'Hello world',
-  html: '<b>Hello world</b>'
-};
 
 var app = express();
 
 app.use(express.static('public'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
@@ -33,17 +32,49 @@ app.get('*', function (req, res) {
 });
 
 app.post('/start-submit', function(req, res) {
+  var body =
+    'Name: ' +
+    '<p>' + req.body.name + '</p>' +
+    'Company: ' +
+    '<p>' + req.body.company + '</p>' +
+    'Title: ' +
+    '<p>' + req.body.title + '</p>' +
+    'Phone: ' +
+    '<p>' + req.body.phone + '</p>' +
+    'Email: ' +
+    '<p>' + req.body.email + '</p>' +
+    'Additional Detail: ' +
+    '<p>' + req.body.details + '</p>';
+
+  var text =
+    'Name: ' + req.body.name +
+    'Company: ' + req.body.company +
+    'Title: ' + req.body.title +
+    'Phone: ' + req.body.phone +
+    'Email: ' + req.body.email +
+    'Additional Detail: ' + req.body.details;
+
+  var email = {
+    from: 'webform@propellerlabs.co',
+    to: 'john@propellerlabs.co',
+    subject: 'Start a project submission',
+    text: text,
+    html: body
+  };
+
   client.sendMail(email, function(err, info){
-      if (err ){
-        console.log(error);
-      }
-      else {
-        console.log('Message sent: ' + info.response);
-      }
+    if (err){
+      console.log(err);
+    }
+    else {
+      console.log('Message sent: ' + info.response);
+    }
   });
+  res.redirect('/');
 });
 
 app.post('/consultation-submit', function(req, res) {
+  console.log('req', req);
   console.log('consultation submit');
 });
 
